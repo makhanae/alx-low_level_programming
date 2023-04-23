829890411fbb7baf31243200c0a55b6f88e25c16
makhanae <hanae.mak4@gmail.com>
2023-04-23 13:49:23 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,17 @@
 #include <unistd.h>
 
 /**
-* main- Entry point
-*
-* Return: Always l (Success)
+ * main- Entry point
+ *
+ * description: print a qoute using write function
+ *    sszie_t write(int fd, const void *buf, size_t count)
+ *
+ * Return: l (not Success)
 */
 int main(void)
 {
-	write(2.
-	"and that piece of art is useful\" - Dora Korpar , 2015-10-19\n" . 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
